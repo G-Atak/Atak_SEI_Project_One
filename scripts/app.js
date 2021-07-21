b58b428c117cc6f456f9ac81c43abd3245c4e5e0
Atak <machokatak@yahoo.com>
2021-07-21 09:32:23 +0100

    I wrote a function to create grid, and then invoke the function, but the grid don't show in the gameboard. I have added two classlist to HTML document for frogHome and frogStartPosition and styled them in CSS. However, these won't show in the gameboard until I get the grid to show and work. After standup this morning, I will approach instructors for assistant.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
-console.log('Frogger Game')
+const boardGrid = document.querySelector('.boardGrid')
+console.log(boardGrid)
+
+const frogStartPosition = document.createElement('.FrogStartPosition')
+document.classList.add(frogStartPosition)
+const frogHome = document.createElement('.frogHome')
+document.classList.add(frogHome)
+const gridWidth = 20
+const totalCells = gridWidth * gridWidth
+
+function buildGridBoard() {
+  for (let i = 0; i < totalCells; i++){
+    const cell = document.createElement('div')
+    boardGrid.appendChild(cell)
+    console.log(buildGridBoard[i])
+  }
+}
+buildGridBoard()
+
+
+
+
