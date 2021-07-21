8986b35ff3a7a636a90f683ffbbdfda516f52170
Atak <machokatak@yahoo.com>
2021-07-21 11:35:41 +0100

    Grid is now set up. Next step is to create class for frog and assign it a position on the grid. I will then use the switch statement to get the frog moving. Next, I am going to create different arrays for cars moving right and cars moving left.

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,38 @@
 const boardGrid = document.querySelector('.boardGrid')
-console.log(boardGrid)
+const cells = []
+// console.log(boardGrid)
+// const startFrogger = document.querySelector('#startFrogger')
 
-const frogStartPosition = document.createElement('.FrogStartPosition')
-document.classList.add(frogStartPosition)
-const frogHome = document.createElement('.frogHome')
-document.classList.add(frogHome)
-const gridWidth = 20
+// const frogStartPosition = document.createElement('.FrogStartPosition')
+// document.classList.add(frogStartPosition)
+// const frogHome = document.createElement('.frogHome')
+// document.classList.add(frogHome)
+// const frog = document.createElement('.frog')
+// document.classList.add(frog)
+
+
+
+const gridWidth = 10
 const totalCells = gridWidth * gridWidth
 
+//function for creating grid cells on the grid board
 function buildGridBoard() {
   for (let i = 0; i < totalCells; i++){
     const cell = document.createElement('div')
     boardGrid.appendChild(cell)
-    console.log(buildGridBoard[i])
+    cells.push(cell)
+    cell.textContent = i
   }
 }
 buildGridBoard()
+
+//  Creating function for starting game
+// function handlefroggerStart(){
+//   console.log('clicked')
+
+// }
+// Adding eventlistener to the froggerStart handle
+// startFrogger.addEventListener('click',handlefroggerStart)
 
 
 
